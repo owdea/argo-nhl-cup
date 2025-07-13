4e879e0bc8220620a4f3c34b3cd5c0ecdc665853
Ondřej Pelcl <ondrejpel@seznam.cz>
2025-07-13 14:14:29 +0200

    Revert "Debuggin Tailwind usage"
    This reverts commit 9ed683412ac48bfdc49f59f2f71c2f9467cd6ecb.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
-import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwind(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
