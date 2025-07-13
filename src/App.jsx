8c300e6d67be3ab9946e317853e30157cbfc5768
Ondřej Pelcl <ondrejpel@seznam.cz>
2025-07-13 14:14:41 +0200

    Revert "Added Tailwind and Postcss"
    This reverts commit d524ebfb1c74ad5283de0093858a92b3d14ddb28.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 import GlobalSettingsForm from "./components/settings/GlobalSettingsForm.jsx";
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import './index.scss'
+import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
