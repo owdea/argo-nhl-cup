bd8ac39344b27deeda3253cf12fe58685d679ca7
Ondřej Pelcl <ondrejpel@seznam.cz>
2025-07-20 18:58:31 +0200

    Všededopiči

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 import './css/main.css'
-import PlayersSettingsForm from "./components/settings/PlayersSettingsForm.jsx"
-import GlobalSettingsForm from "./components/settings/GlobalSettingsForm.jsx"
-import TableRegularSeason from "./components/season/tableRegularSeason.jsx"
-import CreateNewSeason from "./components/season/createNewSeason.jsx"
 
 function App() {
     return (
         <>
-            <PlayersSettingsForm />
-            <GlobalSettingsForm />
-            <TableRegularSeason />
-            <CreateNewSeason />
+            TEST
         </>
     )
 }
--- src/components/season/tableRegularSeason.jsx
+++
@@ -1,9 +0,0 @@
-
-const TableRegularSeason = () => {
-
-    return (
-        <></>
-    )
-}
-
-export default TableRegularSeason;
